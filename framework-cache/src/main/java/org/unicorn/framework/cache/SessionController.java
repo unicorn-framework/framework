045c98313c4d4464af4e03819a1e9b1a316bc535
谢斌 <xiebin163126@163.com>
2017-09-20 05:30:30 +0800

    session共享

--- framework-cache/src/main/java/org/unicorn/framework/cache/SessionController.java
+++ framework-cache/src/main/java/org/unicorn/framework/cache/SessionController.java
@@ -1,29 +1,29 @@
-//
-//package org.unicorn.framework.cache;
-//
-//import java.util.UUID;
-//
-//import javax.servlet.http.HttpSession;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-//*
-//*@author xiebin
-//*
-//*/
-//@RestController
-//@RequestMapping("/redis")
-//public class SessionController {
-//	@RequestMapping("/uid")
-//    String uid(HttpSession session) {
-//        UUID uid = (UUID) session.getAttribute("uid");
-//        if (uid == null) {
-//            uid = UUID.randomUUID();
-//        }
-//        session.setAttribute("uid", uid);
-//        return session.getId();
-//    }
-//}
-//
+
+package org.unicorn.framework.cache;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+*
+*@author xiebin
+*
+*/
+@RestController
+@RequestMapping("/redis")
+public class SessionController {
+	@RequestMapping("/uid")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return session.getId();
+    }
+}
+
--- framework-cache/src/test/java/org/framework/redis/TestRedis.java
+++ framework-cache/src/test/java/org/framework/redis/TestRedis.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.unicorn.framework.cache.Application;
 import org.unicorn.framework.cache.cache.CacheService;
