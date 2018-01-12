67f84128f24cf557d68b1592cf8d4388a590cc43
xiebin <xiebin163126@163.com>
2018-01-12 11:08:54 +0800

    框架更新

--- framework-util/src/main/java/org/unicorn/framework/util/common/BigDecimalUtil.java
+++ framework-util/src/main/java/org/unicorn/framework/util/common/BigDecimalUtil.java
@@ -1,17 +1,38 @@
+/**
+ * Title: CookieUtil.java<br/>
+ * Description: <br/>
+ * Copyright: Copyright (c) 2015<br/>
+ * 
+ *
+ */
 package org.unicorn.framework.util.common;
 
 import java.math.BigDecimal;
 
 /**
+ * Description: <br/>
  * 
  * @author xiebin
  *
  */
-public class BigDecimalUtil {
-	  public static BigDecimal bigDecimal2RoundHalfUp(BigDecimal target){
-          return target.setScale(2,BigDecimal.ROUND_HALF_UP);
-}
-   public static BigDecimal bigDecimalDivideRoundHalfUp(BigDecimal divided,BigDecimal divide){
-          return divided.divide(divide,2,BigDecimal.ROUND_HALF_UP);
-}
-}
+public class BigDecimalUtil {  
+  
+	 public static BigDecimal bigDecimal2RoundHalfUp(BigDecimal target){
+	    	return target.setScale(2,BigDecimal.ROUND_HALF_UP);
+	 }
+	 
+	 public static BigDecimal bigDecimalDivideRoundHalfUp(BigDecimal divided,BigDecimal divide){
+	    	return divided.divide(divide,2,BigDecimal.ROUND_HALF_UP);
+	 }
+	 
+	 public static void main(String[] args) {
+		 BigDecimal a=new BigDecimal(10.1);
+		 BigDecimal b=new BigDecimal(5.1);
+		 System.out.println(a.multiply(b));
+		 System.out.println(bigDecimal2RoundHalfUp(a.multiply(b)));
+		
+		 
+		 BigDecimal bg= bigDecimalDivideRoundHalfUp(new BigDecimal(10.1),new BigDecimal(5.1).multiply(new BigDecimal(3)));
+		System.out.println(bigDecimal2RoundHalfUp(bg.add(a)));
+	}
+}  
