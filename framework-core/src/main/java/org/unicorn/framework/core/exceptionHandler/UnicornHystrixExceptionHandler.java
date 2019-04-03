69bff864de7ca11fff06c069ab6110da208451c7
xb <xiebin1631216@163.com>
2019-04-03 16:51:43 +0800

    框架更新

---
+++ framework-core/src/main/java/org/unicorn/framework/core/exceptionHandler/UnicornHystrixExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.unicorn.framework.core.exceptionHandler;
+
+import com.netflix.client.ClientException;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+import org.unicorn.framework.core.SysCode;
+import org.unicorn.framework.core.exception.PendingException;
+
+/**
+ * hystrix异常处理
+ * @author  xiebin
+ */
+public class UnicornHystrixExceptionHandler {
+
+    public void hystrixExceptionHandler( Throwable throwable) throws PendingException {
+        if (throwable instanceof PendingException) {
+            throw (PendingException) throwable;
+        }else if(throwable instanceof HystrixTimeoutException){
+            throw new PendingException(SysCode.SYS_FAIL, "调用超时");
+        }else if(throwable.getCause() instanceof ClientException){
+            throw new PendingException(SysCode.SYS_FAIL, "没有找到可用的服务");
+        }else {
+            throw new PendingException(SysCode.SYS_FAIL, throwable.getMessage());
+        }
+    }
+}
