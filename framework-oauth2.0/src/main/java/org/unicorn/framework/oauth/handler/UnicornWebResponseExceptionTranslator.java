58b5601c0ccc043df5316c391ceec5ac4387df27
xb <xiebin1631216@163.com>
2019-05-06 12:25:55 +0800

    框架更新

--- framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/handler/UnicornWebResponseExceptionTranslator.java
+++ framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/handler/UnicornWebResponseExceptionTranslator.java
@@ -1,5 +1,6 @@
 package org.unicorn.framework.oauth.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
@@ -10,13 +11,21 @@ import org.springframework.stereotype.Component;
  * @since 1.0
  */
 @Component("customWebResponseExceptionTranslator")
+@Slf4j
 public class UnicornWebResponseExceptionTranslator implements WebResponseExceptionTranslator {
     @Override
     public ResponseEntity<OAuth2Exception> translate(Exception e) throws Exception {
+        log.error("异常:",e);
+        if(e instanceof OAuth2Exception){
+            OAuth2Exception oAuth2Exception = (OAuth2Exception) e;
+            return ResponseEntity
+                    .status(oAuth2Exception.getHttpErrorCode())
+                    .body(new UnicornOauthException(oAuth2Exception.getMessage()));
+        }else{
+            return ResponseEntity
+                    .status(400)
+                    .body(new UnicornOauthException(e.getMessage()));
+        }
 
-        OAuth2Exception oAuth2Exception = (OAuth2Exception) e;
-        return ResponseEntity
-                .status(oAuth2Exception.getHttpErrorCode())
-                .body(new UnicornOauthException(oAuth2Exception.getMessage()));
     }
 }
