299eeb649b739e0195d6c14b94d11da78c7e725b
xb <xiebin1631216@163.com>
2019-05-16 15:32:27 +0800

    随机工具

--- framework-util/src/main/java/org/unicorn/framework/util/common/RandomUtils.java
+++ framework-util/src/main/java/org/unicorn/framework/util/common/RandomUtils.java
@@ -6,16 +6,16 @@ import java.util.UUID;
 
 /**
  * 随机数工具类
- * 
+ *
  * @author xiebin
  */
 public class RandomUtils {
 
     /**
      * 获取随机数
-     * 
+     *
      * @param passLength 随机数长度
-     * @param type 随机数类型
+     * @param type       随机数类型
      * @return
      */
     public static String getCode(int passLength, int type) {
@@ -23,26 +23,29 @@ public class RandomUtils {
         StringBuffer sb = new StringBuffer();
         Random r = new Random();
         switch (type) {
-        case 0:
-            buffer = new StringBuffer("0123456789");
-            break;
-        case 1:
-            buffer = new StringBuffer("abcdefghijklmnopqrstuvwxyz");
-            break;
-        case 2:
-            buffer = new StringBuffer("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-            break;
-        case 3:
-            buffer = new StringBuffer("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-            break;
-        case 4:
-            buffer = new StringBuffer("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
-            sb.append(buffer.charAt(r.nextInt(buffer.length() - 10)));
-            passLength -= 1;
-            break;
-        case 5:
-            String s = UUID.randomUUID().toString();
-            sb.append(s.substring(0, 8) + s.substring(9, 13) + s.substring(14, 18) + s.substring(19, 23) + s.substring(24));
+            case 0:
+                buffer = new StringBuffer("0123456789");
+                break;
+            case 1:
+                buffer = new StringBuffer("abcdefghijklmnopqrstuvwxyz");
+                break;
+            case 2:
+                buffer = new StringBuffer("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+                break;
+            case 3:
+                buffer = new StringBuffer("123456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ");
+                break;
+            case 4:
+                buffer = new StringBuffer("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+                break;
+            case 5:
+                buffer = new StringBuffer("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+                sb.append(buffer.charAt(r.nextInt(buffer.length() - 10)));
+                passLength -= 1;
+                break;
+            case 6:
+                String s = UUID.randomUUID().toString();
+                sb.append(s.substring(0, 8) + s.substring(9, 13) + s.substring(14, 18) + s.substring(19, 23) + s.substring(24));
         }
 
         if (type != 5) {
@@ -53,8 +56,8 @@ public class RandomUtils {
         }
         return sb.toString();
     }
-    
-    public static String getFlowNum(String businessPrefix){
-    	return businessPrefix+"_"+ DateUtils.format(new Date(), "yyyyMMddHHmmss")+"_"+getCode(5,0);
+
+    public static String getFlowNum(String businessPrefix) {
+        return businessPrefix + "_" + DateUtils.format(new Date(), "yyyyMMddHHmmss") + "_" + getCode(5, 0);
     }
 }
