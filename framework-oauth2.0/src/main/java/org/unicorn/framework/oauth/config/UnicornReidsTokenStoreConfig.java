22b6b696ccd82c4daed0a00ef834b1fc07878cd0
xb <xiebin1631216@163.com>
2019-06-04 09:39:56 +0800

    框架更新

--- framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/config/UnicornReidsTokenStoreConfig.java
+++ framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/config/UnicornReidsTokenStoreConfig.java
@@ -1,22 +1,19 @@
 package org.unicorn.framework.oauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-import org.unicorn.framework.oauth.store.UnicornRedisTokenStore;
 
 /**
  * @author xiebin
  * @since 1.0
  */
 @Configuration
-//@ConditionalOnExpression("${unicorn.security.oauth2.authorizationServer:false}")
-//@ConditionalOnProperty(prefix = "unicorn.security.oauth2", name = "storeType", havingValue = "redis")
+@ConditionalOnProperty(prefix = "unicorn.security.oauth2", name = "storeType", havingValue = "redis")
 public class UnicornReidsTokenStoreConfig {
     /**
      * redis连接工厂
