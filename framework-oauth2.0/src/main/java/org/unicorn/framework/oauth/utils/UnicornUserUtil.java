83682524613f39e9a37f58f98b3b06d59ee277cc
xb <xiebin1631216@163.com>
2019-07-25 15:58:48 +0800

    添加工具类

---
+++ framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/utils/UnicornUserUtil.java
@@ -0,0 +1,30 @@
+package org.unicorn.framework.oauth.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.unicorn.framework.oauth.dto.UnicornUser;
+
+import java.security.Principal;
+
+/**
+ * @author xiebin
+ */
+@Slf4j
+public class UnicornUserUtil {
+    public static Long getUserId(Principal  principal){
+        try{
+            OAuth2Authentication oo = (OAuth2Authentication) principal;
+            Long userId = null;
+            if (oo != null) {
+                UnicornUser user = (UnicornUser) oo.getUserAuthentication().getPrincipal();
+                userId = user.getId();
+            }
+            return userId;
+        }catch(Exception e){
+            log.warn("获取用户信息异常",e);
+            return null;
+        }
+
+
+    }
+}
