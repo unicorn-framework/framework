eb2301a840fa14825c6088a7548fc8d99810197b
xb <xiebin1631216@163.com>
2019-07-25 16:19:51 +0800

    修改工具类

--- framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/utils/UnicornUserUtil.java
+++ framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/utils/UnicornUserUtil.java
@@ -1,6 +1,8 @@
 package org.unicorn.framework.oauth.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.unicorn.framework.oauth.dto.UnicornUser;
 
@@ -11,8 +13,8 @@ import java.security.Principal;
  */
 @Slf4j
 public class UnicornUserUtil {
-    public static Long getUserId(Principal  principal){
-        try{
+    public static Long getUserId(Principal principal) {
+        try {
             OAuth2Authentication oo = (OAuth2Authentication) principal;
             Long userId = null;
             if (oo != null) {
@@ -20,8 +22,23 @@ public class UnicornUserUtil {
                 userId = user.getId();
             }
             return userId;
-        }catch(Exception e){
-            log.warn("获取用户信息异常",e);
+        } catch (Exception e) {
+            log.warn("获取用户信息异常", e);
+            return null;
+        }
+    }
+
+    public static Long getUserId() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Long userId = null;
+            if (authentication != null) {
+                UnicornUser user = (UnicornUser) authentication.getPrincipal();
+                userId = user.getId();
+            }
+            return userId;
+        } catch (Exception e) {
+            log.warn("获取用户信息异常", e);
             return null;
         }
 
