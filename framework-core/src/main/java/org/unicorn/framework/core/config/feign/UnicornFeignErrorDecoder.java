53cb8e5e8b013cf1471095b0772cec0882b84c02
xb <xiebin1631216@163.com>
2019-07-31 11:18:05 +0800

    打出 methedKey

--- framework-core/src/main/java/org/unicorn/framework/core/config/feign/UnicornFeignErrorDecoder.java
+++ framework-core/src/main/java/org/unicorn/framework/core/config/feign/UnicornFeignErrorDecoder.java
@@ -38,7 +38,7 @@ public class UnicornFeignErrorDecoder implements ErrorDecoder {
                 if(methodKey.contains("AuthTokenClient#postAccessToken(String,Map)")){
                     return new HystrixBadRequestException("用户信息错误");
                 }
-                return new HystrixBadRequestException("请求参数错误");
+                return new HystrixBadRequestException("请求参数错误 :"+methodKey);
             }
             // 这里直接拿到feign服务端抛出的异常信息
             message = Util.toString(response.body().asReader());
