4918f2000e46f795801f9a06742c22b46e9316c5
xb <xiebin1631216@163.com>
2019-08-07 11:17:43 +0800

    添加用户标识

--- framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/dto/UnicornUser.java
+++ framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/dto/UnicornUser.java
@@ -11,16 +11,21 @@ import java.util.Collection;
  */
 @Data
 public class UnicornUser extends User {
+    //用户ID
     private Long id;
-    public UnicornUser(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+    //是否新用户
+    private Boolean newUser;
 
+    public UnicornUser(Long id, Boolean newUser, String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, true, true, true, true, authorities);
         this.id=id;
+        this.newUser=newUser;
     }
 
-    public UnicornUser(Long id, String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+    public UnicornUser(Long id, Boolean newUser,String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
         this.id=id;
+        this.newUser=newUser;
     }
 
 
