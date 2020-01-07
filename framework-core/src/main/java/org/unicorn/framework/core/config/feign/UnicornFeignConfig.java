085b07c8e0d626285f748e18762eb8739614d602
xb <xiebin1631216@163.com>
2020-01-07 16:51:23 +0800

    去掉无效引用

--- framework-core/src/main/java/org/unicorn/framework/core/config/feign/UnicornFeignConfig.java
+++ framework-core/src/main/java/org/unicorn/framework/core/config/feign/UnicornFeignConfig.java
@@ -2,7 +2,6 @@ package org.unicorn.framework.core.config.feign;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import io.jmnarloch.spring.cloud.ribbon.support.RibbonFilterContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
