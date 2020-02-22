d32c076249578d5e5bf65be7220c0bb0f1dab29e
xiebin <xiebin163126@163.com>
2020-02-22 15:01:39 +0800

    加密

--- framework-base/src/main/java/org/unicorn/framework/base/base/UnicornPasswordEncoder.java
+++ framework-base/src/main/java/org/unicorn/framework/base/base/UnicornPasswordEncoder.java
@@ -1,22 +1,23 @@
 package org.unicorn.framework.base.base;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * @author  xiebin
+ * @author xiebin
  */
 public class UnicornPasswordEncoder implements PasswordEncoder {
+    private static final BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+
     @Override
     public String encode(CharSequence charSequence) {
-        return charSequence.toString();
+        return bcryptPasswordEncoder.encode(charSequence);
     }
-
-
 
 
     @Override
     public boolean matches(CharSequence charSequence, String s) {
-        return s.equalsIgnoreCase(charSequence.toString());
+        return bcryptPasswordEncoder.matches(charSequence.toString(), s);
     }
 
 
