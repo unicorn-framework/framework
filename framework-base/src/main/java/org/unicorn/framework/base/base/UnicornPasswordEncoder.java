58bffbf07ace27e411419f58e5030381057f9af8
xiebin <xiebin163126@163.com>
2020-02-22 15:31:10 +0800

    修改加密类

--- framework-base/src/main/java/org/unicorn/framework/base/base/UnicornPasswordEncoder.java
+++ framework-base/src/main/java/org/unicorn/framework/base/base/UnicornPasswordEncoder.java
@@ -2,6 +2,7 @@ package org.unicorn.framework.base.base;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.unicorn.framework.base.constants.UnicornConstants;
 
 /**
  * @author xiebin
@@ -9,15 +10,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class UnicornPasswordEncoder implements PasswordEncoder {
     private static final BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
 
+
     @Override
     public String encode(CharSequence charSequence) {
+        //是app端不加密  权宜之计
+        if (charSequence.toString().startsWith(UnicornConstants.APP_ECODE_PRE)) {
+            return charSequence.toString();
+        }
         return bcryptPasswordEncoder.encode(charSequence);
     }
 
 
     @Override
-    public boolean matches(CharSequence charSequence, String s) {
-        return bcryptPasswordEncoder.matches(charSequence.toString(), s);
+    public boolean matches(CharSequence charSequence, String encodeString) {
+        if (encodeString.contains(UnicornConstants.APP_ECODE_PRE)) {
+            return charSequence.equals(encodeString.replaceFirst(UnicornConstants.APP_ECODE_PRE, ""));
+        }
+        return bcryptPasswordEncoder.matches(charSequence.toString(), encodeString);
     }
 
 
