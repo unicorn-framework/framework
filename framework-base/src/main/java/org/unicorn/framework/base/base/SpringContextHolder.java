7a9a0c577407e4b40aeb4a794bfedbf33ab9e2cf
xb <xiebin1631216@163.com>
2020-06-19 15:07:41 +0800

    增加获取request对象的方法

--- framework-base/src/main/java/org/unicorn/framework/base/base/SpringContextHolder.java
+++ framework-base/src/main/java/org/unicorn/framework/base/base/SpringContextHolder.java
@@ -2,31 +2,38 @@
  * Title: SpringContextHolder.java<br/>
  * Description: <br/>
  * Copyright: Copyright (c) 2015<br/>
- * 
- *
  */
 package org.unicorn.framework.base.base;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Title: SpringContextHolder<br/>
  * Description: <br/>
- * 
- * @author xiebin
  *
+ * @author xiebin
  */
 
 public class SpringContextHolder implements ApplicationContextAware {
-    
+
     private static ApplicationContext applicationContext;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext)
             throws BeansException {
         SpringContextHolder.applicationContext = applicationContext;
+    }
+
+    public static HttpServletRequest getRequest() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        return request;
     }
 
     public static ApplicationContext getApplicationContext() {
@@ -36,7 +43,7 @@ public class SpringContextHolder implements ApplicationContextAware {
     public static Object getBean(String beanName) {
         return applicationContext.getBean(beanName);
     }
-    
+
     public static Object getBean(Class<?> clazz) {
         return applicationContext.getBean(clazz);
     }
