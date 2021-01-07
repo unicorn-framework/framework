587b3b06ad357c4cbe50b2fdf58c3b38deadf5a2
xb <xiebin1631216@163.com>
2021-01-07 11:30:41 +0800

    忽略安全检查

--- framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/config/UnicornSecurityConfig.java
+++ framework-oauth2.0/src/main/java/org/unicorn/framework/oauth/config/UnicornSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.unicorn.framework.oauth.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,6 +11,10 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.unicorn.framework.oauth.properties.OAuth2Properties;
+import org.unicorn.framework.util.json.JsonUtils;
+
+import java.util.List;
 
 /**
  * 安全配置
@@ -18,8 +24,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
  */
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Slf4j
 public class UnicornSecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Autowired
+    private OAuth2Properties oauth2Properties;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -27,11 +35,17 @@ public class UnicornSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+        List<String> permitList = oauth2Properties.getPermitAlls();
+        permitList.add("/oauth/**");
+        permitList.add("/**/error");
+        log.info("需要忽略token安全检查===" + JsonUtils.toJson(permitList));
         web.ignoring().antMatchers("/static/**");
         web.ignoring().antMatchers("*.html");
         web.ignoring().antMatchers("/**/**.html", "/**/**.js", "/**/**.css", "/**/**.ico", "/**/**.ttf");
         web.ignoring().antMatchers("/static/**");
         web.ignoring().antMatchers("/**/swagger**", "/**/v2/**");
+        web.ignoring().antMatchers(permitList.stream().toArray(String[]::new));
+
 
     }
 
